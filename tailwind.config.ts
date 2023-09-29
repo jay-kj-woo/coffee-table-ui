c30b671c4c9b4e196116f9bf53660cbc23f53f16
Jay Woo <jw_1020@hotmail.com>
2023-09-29 14:52:14 -0400

    feat: adds some font and custom theme settings for the app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -7,7 +7,31 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      main: {
+        500: '#2b2c6e',
+        300: '#53548a',
+      },
+      dark: {
+        500: '#19142b',
+      },
+      light: {
+        500: '#f5f6f8',
+      },
+      Walnut: '#582f11',
+      Oak: '#DDB586',
+      BlackWood: '#0A0401',
+      Espresso: '#442109',
+      Eclipse: '#1D1D1D',
+      Twilight: '#999999',
+      Solstice: '#F8F8F8',
+    },
     extend: {
+      fontFamily: {
+        campton: ['var(--font-campton)'],
+        larsseit: ['var(--font-larsseit)'],
+        quincy: ['var(--font-quincy)'],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -16,5 +40,5 @@ const config: Config = {
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
